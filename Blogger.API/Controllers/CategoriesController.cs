1a6aeb6f947bc295d57c0c5d36677a0677be4b40
Zhaoyi (Astor) Song <ab13522095821@gmail.com>
2025-09-18 13:07:31 -0400

    Created POST Category action.

---
+++ Blogger.API/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using Blogger.API.Data;
+using Blogger.API.Models.Domain;
+using Blogger.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogger.API.Controllers
+{
+    // https://localhost:xxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // 
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            // Domain Model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
