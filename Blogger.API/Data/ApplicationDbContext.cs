8f8aae1ec3bb18b52de291b1c971a054b02349d5
Zhaoyi (Astor) Song <ab13522095821@gmail.com>
2025-09-18 12:53:53 -0400

    Set up the database in SqlServer.

---
+++ Blogger.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blogger.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.Domain.BlogPost> BlogPosts { get; set; }
+        public DbSet<Models.Domain.Category> Categories { get; set; }
+    }
+}
